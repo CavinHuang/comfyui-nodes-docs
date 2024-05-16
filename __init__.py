c266cbca537e2e4e49ac70472084c48af1424a07
cavinHuang <sujinw@qq.com>
2024-05-16 23:24:21 +0800

    feat: 增加节点数据暂时

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
 import os
+from server import PromptServer
+from aiohttp import web
 
 WEB_DIRECTORY = "./web/comfyui"
 
 __all__ = ['WEB_DIRECTORY']
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@PromptServer.instance.routes.get("/customnode/getNodeInfo")
+async def fetch_customnode_node_info(request):
+  node_name = request.rel_url.query["nodeName"]
+
+  file_path = os.path.join(CURRENT_DIR, 'docs', node_name + '.md')
+  if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+      return web.json_response({"content": file.read()})
+  return web.json_response({"content": ""})
--- web/comfyui/index.js
+++ web/comfyui/index.js
@@ -1,8 +1,15 @@
 import { app } from "../../scripts/app.js";
+import { marked } from './marked.js'
 
 console.log('app', app)
+/**
+ * 1: {
+ * x: [0,1]
+ * y: [0,1]
+ * }
+ */
+const cacheNodePositonMap = new Map();
 const drawDocIcon = function(node, orig, restArgs) {
-  debugger
   let ctx = restArgs[0];
 	const r = orig?.apply?.(node, restArgs);
 
@@ -16,8 +23,17 @@ const drawDocIcon = function(node, orig, restArgs) {
     const sz = ctx.measureText(docIcon);
     ctx.beginPath();
     ctx.fillStyle = fgColor;
-    ctx.fillText(docIcon, node.size[0] - sz.width - 6, -LiteGraph.NODE_TITLE_HEIGHT + 22);
+    const x = node.size[0] - sz.width - 6;
+    const y = -LiteGraph.NODE_TITLE_HEIGHT + 22;
+    ctx.fillText(docIcon, x, y);
     ctx.restore();
+
+    const boundary = node.getBounding();
+    const [ x1, y1, width, height ] = boundary
+    cacheNodePositonMap.set(node.id, {
+      x: [x1 + x, x1 + x + sz.width],
+      y: [y1 , y1 + 22]
+    })
 
     if (node.has_errors) {
       ctx.save();
@@ -31,18 +47,49 @@ const drawDocIcon = function(node, orig, restArgs) {
   return r
 }
 
+/**
+ * {
+ *  nodeId: Element
+ * }
+ */
+const nodeDocsEleMap = new Map()
+const showNodeDocs = async function(node) {
+  const ele = nodeDocsEleMap.get(node.id)
+  if(ele) {
+    ele.style.display = 'block'
+    return
+  }
+  const divWrap = document.createElement('div')
+  divWrap.style.position = 'absolute'
+
+  const [nLeft, nTop, nWidth, nHeight] = node.getBounding()
+  divWrap.style.left = (nLeft + nWidth + 20) + 'px'
+  divWrap.style.top = '20px'
+  divWrap.style.width = '800px'
+  divWrap.style.maxHeight = window.innerHeight - 100 + 'px'
+  divWrap.style.backgroundColor = 'rgba(0,0,0, 1)'
+  divWrap.style.color = 'white'
+  divWrap.style.padding = '10px'
+  divWrap.style.borderRadius = '10px'
+  divWrap.style.zIndex = '9999'
+  divWrap.style.overflowX = 'hidden'
+  divWrap.style.overflowY = 'auto'
+
+  document.body.appendChild(divWrap)
+
+  const res = await fetch('/customnode/getNodeInfo?nodeName=' + node.type)
+  const jsonData = await res.json()
+  console.log(marked, jsonData)
+  const html = marked.parse(jsonData.content);
+
+  divWrap.innerHTML = html || node.description || '暂无文档'
+
+  nodeDocsEleMap.set(node.id, divWrap)
+}
+
 app.registerExtension({
   name: 'Leo.NodeDocs',
   setup() {
-
-    const a = app.canvas.processMouseDown
-    console.log('🚀 ~ setup ~ a:', a)
-
-    app.canvas.processMouseDown = function(e) {
-      console.log('processMouseDown', e)
-      a.apply(app.canvas, arguments)
-    }
-
   },
   nodeCreated: function(node, app) {
     if(!node.doc_enabled) {
@@ -64,7 +111,21 @@ app.registerExtension({
     const oDb = node.onMouseDown
     node.onMouseDown = function(e) {
       oDb?.apply(node, arguments)
-      console.log('onMouseDown', e)
+      const { canvasX, canvasY } = e
+
+      const nodePos = cacheNodePositonMap.get(node.id)
+      console.log("🚀 ~ loadedGraphNode ~ nodePos:", nodePos)
+      console.log("🚀 ~ loadedGraphNode ~ nodePos:", e)
+      console.log("🚀 ~ loadedGraphNode ~ nodePos:", node.getBounding())
+      if(nodePos) {
+        const {x, y} = nodePos
+        console.log(canvasX, canvasY, x, y)
+        if(canvasX >= x[0] && canvasX <= x[1] && canvasY >= y[0] && canvasY <= y[1]) {
+          console.log('🚀 ~ node.onMouseDown ~ nodePos11111', nodePos)
+          showNodeDocs(node)
+          // app.showNodeDocs(node)
+        }
+      }
     }
 	},
 });
