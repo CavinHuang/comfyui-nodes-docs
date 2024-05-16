5d0e132beb7cefaf53b89512efd9f25ab2cb1e1e
cavinHuang <sujinw@qq.com>
2024-05-17 07:47:04 +0800

    feat: 增加文档显示

--- web/comfyui/index.js
+++ web/comfyui/index.js
@@ -53,6 +53,18 @@ const drawDocIcon = function(node, orig, restArgs) {
  * }
  */
 const nodeDocsEleMap = new Map()
+/**
+ * 当前激活的文档
+ * @type {Element | null}
+ */
+let activeDocsEle = null
+
+const hideActiveDocs = function() {
+  if(activeDocsEle) {
+    activeDocsEle.style.display = 'none'
+  }
+}
+
 const showNodeDocs = async function(node) {
   const ele = nodeDocsEleMap.get(node.id)
   if(ele) {
@@ -66,30 +78,123 @@ const showNodeDocs = async function(node) {
   divWrap.style.left = (nLeft + nWidth + 20) + 'px'
   divWrap.style.top = '20px'
   divWrap.style.width = '800px'
-  divWrap.style.maxHeight = window.innerHeight - 100 + 'px'
-  divWrap.style.backgroundColor = 'rgba(0,0,0, 1)'
+  divWrap.style.height = window.innerHeight - 100 + 'px'
+  divWrap.style.backgroundColor = 'var(--comfy-menu-bg)'
   divWrap.style.color = 'white'
   divWrap.style.padding = '10px'
   divWrap.style.borderRadius = '10px'
   divWrap.style.zIndex = '9999'
-  divWrap.style.overflowX = 'hidden'
-  divWrap.style.overflowY = 'auto'
+  divWrap.style.overflow = 'hidden'
+  divWrap.style.boxShadow = '3px 3px 8px rgba(0, 0, 0, 0.4)'
 
   document.body.appendChild(divWrap)
+
+  const buttonClose = document.createElement('button')
+  /**
+   * background-color: rgba(0, 0, 0, 0);
+    padding: 0;
+    border: none;
+    cursor: pointer;
+    font-size: inherit;
+   */
+  buttonClose.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+  buttonClose.style.padding = '0'
+  buttonClose.style.border = 'none'
+  buttonClose.style.cursor = 'pointer'
+  buttonClose.style.fontSize = '36px'
+  buttonClose.innerText = '×'
+  buttonClose.className = 'comfy-close-menu-btn'
+
+  buttonClose.onclick = function() {
+    divWrap.style.display = 'none'
+  }
+
+  const divButtonWrap = document.createElement('div')
+  
+  divButtonWrap.style.display = 'flex'
+  divButtonWrap.style.justifyContent = 'flex-end'
+  divButtonWrap.style.height = '32px'
+  divButtonWrap.appendChild(buttonClose)
+
+  const divContentWrap = document.createElement('div')
+  divContentWrap.style.background = 'var(--comfy-input-bg)'
+  divContentWrap.style.height = 'calc(100% - 44px)'
+  divContentWrap.style.padding = '10px'
+  divContentWrap.style.borderRadius = '10px'
+  divContentWrap.style.overflowX = 'hidden'
+  divContentWrap.style.overflowY = 'auto'
+
+  divWrap.appendChild(divButtonWrap)
+  divWrap.appendChild(divContentWrap)
+
 
   const res = await fetch('/customnode/getNodeInfo?nodeName=' + node.type)
   const jsonData = await res.json()
   console.log(marked, jsonData)
   const html = marked.parse(jsonData.content);
 
-  divWrap.innerHTML = html || node.description || '暂无文档'
+  divContentWrap.innerHTML = html || node.description || '暂无文档'
+
+  if (activeDocsEle) {
+    hideActiveDocs()
+  }
+  activeDocsEle = divWrap
 
   nodeDocsEleMap.set(node.id, divWrap)
+}
+
+/**
+ * 节流函数
+ */
+const throttle = function(fn, delay) {
+  let lastTime = 0
+  return function() {
+    const now = Date.now()
+    if(now - lastTime > delay) {
+      fn.apply(this, arguments)
+      lastTime = now
+    }
+  }
 }
 
 app.registerExtension({
   name: 'Leo.NodeDocs',
   setup() {
+    app.canvasEl.addEventListener('click', function(e) {
+      console.log('🚀 ~ app.canvasEl.addEventListener ~ e', e)
+      // 排除点击文档区域
+      const { clientX, clientY } = e
+      console.log("🚀 ~ app.canvasEl.addEventListener ~ clientX:", clientX, clientY)
+      let isClickDoc = false
+      cacheNodePositonMap.forEach((value, key) => {
+        const {x, y} = value
+        console.log("🚀 ~ app.canvasEl.addEventListener ~ x, y", x, y)
+        console.log(clientX < x[0], clientX > x[1], clientY < y[0], clientY > y[1]);
+        // 排除文档图标区域
+        if(clientX >= x[0] && clientX <= x[1] && clientY >= y[0] && clientY <= y[1]) {
+          isClickDoc = true
+          return
+        }
+      })
+      if(!isClickDoc) {
+        hideActiveDocs()
+      }
+    }, false)
+
+    // window resize重新计算所有文档的位置
+    window.addEventListener('resize', throttle(() => {
+      cacheNodePositonMap.forEach((value, key) => {
+        const node = app.graph.getNodeById(key)
+        if(node) {
+          const [nLeft, nTop, nWidth, nHeight] = node.getBounding()
+          const ele = nodeDocsEleMap.get(key)
+          if(ele) {
+            ele.style.left = (nLeft + nWidth + 20) + 'px'
+          }
+        }
+      })
+    }, 1000))
+
   },
   nodeCreated: function(node, app) {
     if(!node.doc_enabled) {
@@ -124,6 +229,9 @@ app.registerExtension({
           console.log('🚀 ~ node.onMouseDown ~ nodePos11111', nodePos)
           showNodeDocs(node)
           // app.showNodeDocs(node)
+          e.preventDefault()
+          e.stopPropagation()
+          return false
         }
       }
     }
