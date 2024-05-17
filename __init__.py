6cfba99c596bc5221442d9d9e0479568ce59d1d6
cavinHuang <huangchunmao1993@gmail.com>
2024-05-17 17:44:12 +0800

    fix: file encoding

--- __init__.py
+++ __init__.py
@@ -10,10 +10,17 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @PromptServer.instance.routes.get("/customnode/getNodeInfo")
 async def fetch_customnode_node_info(request):
-  node_name = request.rel_url.query["nodeName"]
+  try:
+    node_name = request.rel_url.query["nodeName"]
 
-  file_path = os.path.join(CURRENT_DIR, 'docs', node_name + '.md')
-  if os.path.exists(file_path):
-    with open(file_path, 'r') as file:
-      return web.json_response({"content": file.read()})
-  return web.json_response({"content": ""})
+    if not node_name:
+      return web.json_response({"content": ""})
+
+    file_path = os.path.join(CURRENT_DIR, 'docs', node_name + '.md')
+    if os.path.exists(file_path):
+      with open(file_path, 'r', encoding='utf-8') as file:
+        return web.json_response({"content": file.read()})
+    else:
+      return web.json_response({"content": ""})
+  except Exception as e:
+    return web.json_response({"content": ""})
