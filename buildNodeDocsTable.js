78cf027c07cd75940818930b5e3f8f1b63b04772
cavinHuang <huangchunmao1993@gmail.com>
2024-05-20 19:29:16 +0800

    fix: fix node doc list

--- buildNodeDocsTable.js
+++ buildNodeDocsTable.js
@@ -7,13 +7,14 @@ const docsDir = path.join(__dirname, './docs');
 
 const files = fs.readdirSync(docsDir);
 
-let tableMd = `| Node | Description | Link  | \n| --- | --- | --- | \n`;
+let tableMd = `| Node | Description | Link  | Done | \n | --- | --- | --- | --- | \n`;
 
 files.forEach((file) => {
   if (file.endsWith('.md')) {
     const fileName = file.split('.')[0];
+    const fileSize = fs.statSync(path.join(docsDir, file)).size;
     const link = file.replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/ /g, '%20');
-    tableMd += `| ${fileName} | ${fileName} | [${fileName}](docs/${link}) | \n`;
+    tableMd += `| ${fileName} | ${fileName} | [${fileName}](docs/${link}) | - [ ${ fileSize ? 'x' : '' } ] | \n`;
   }
 })
 
