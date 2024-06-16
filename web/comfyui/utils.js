6b02697283c0528501ccbe32e68a184ee0b86627
Leo <huangchunmao1993@gmail.com>
2024-06-16 20:54:46 +0800

    feat: add move and resize

--- web/comfyui/utils.js
+++ web/comfyui/utils.js
@@ -12,3 +12,15 @@ export const throttle = function(fn, delay) {
     }
   }
 }
+
+ 
+export function svgToBase64(svgString) {
+  // 对SVG字符串进行编码
+  const encodedSvg = encodeURIComponent(svgString).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+      return String.fromCharCode('0x' + p1);
+  });
+
+  // 将编码后的字符串转换为base64
+  const base64 = btoa(encodedSvg);
+  return 'data:image/svg+xml;base64,' + base64;
+}
