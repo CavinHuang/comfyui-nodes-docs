53a055ebdb02d449de14b6ce44afe1e5bb7de24a
Leo <huangchunmao1993@gmail.com>
2024-06-29 18:41:18 +0800

    fix: 删除文件

--- nodeGen.js
+++ nodeGen.js
@@ -26,81 +26,115 @@ const needAddFileContent = {};
 // const nodeDocs = fs.readdirSync(nodeDocsDir);
 
 const windowsPathIgnore = [
-    // ['/', '%2F'],
-    ['\\', '%5C'],
-    [':', '%3A'],
-    ['*', '%2A'],
-    ['|', '%7C'],
-    ['<', '%3C'],
-    ['>', '%3E'],
-    ['"', '%22'],
-    ['?', '%3F']]
+  // ['/', '%2F'],
+  ['\\', '%5C'],
+  [':', '%3A'],
+  ['*', '%2A'],
+  ['|', '%7C'],
+  ['<', '%3C'],
+  ['>', '%3E'],
+  ['"', '%22'],
+  ['?', '%3F']
+]
 
-  const repairePath = (fileName) => {
-    let name = fileName;
-    windowsPathIgnore.forEach((item) => {
-      name = name.replace(item[0], item[1]);
-    })
-    return name;
-  }
+const repairePath = (fileName) => {
+  let name = fileName;
+  windowsPathIgnore.forEach((item) => {
+    name = name.replace(item[0], item[1]);
+  })
+  return name;
+}
 
 const nodeGen = (nav) => {
-    const NodeDocumentation = nav[4]['Node Documentation']
-    console.log(NodeDocumentation);
+  const NodeDocumentation = nav[4]['Node Documentation']
+  // console.log(NodeDocumentation);
 
-    for (let i = 0; i < NodeDocumentation.length; i++) {
-        const plugin = NodeDocumentation[i];
-        // 获取object的key
-        const pluginName = Object.keys(plugin)[0];
-        // 获取object的value
-        const pluginNodes = plugin[pluginName][2]['Nodes'];
+  for (let i = 0; i < NodeDocumentation.length; i++) {
+    const plugin = NodeDocumentation[i];
+    // 获取object的key
+    const pluginName = Object.keys(plugin)[0];
+    // 获取object的value
+    const pluginNodes = plugin[pluginName][2]['Nodes'] || [];
 
-        const enPluginDoc = path.join(nodesDocsDir, 'en-US', pluginName);
-        const cnPluginDoc = path.join(nodesDocsDir, 'zh-CN', pluginName);
-        console.log(cnPluginDoc);
-        // 获取node的名称
-        for (let j = 0; j < pluginNodes.length; j++) {
-            const node = pluginNodes[j];
+    const enPluginDoc = path.join(nodesDocsDir, 'en-US', pluginName);
+    const cnPluginDoc = path.join(nodesDocsDir, 'zh-CN', pluginName);
 
-            const nodeName = Object.keys(node)[0];
-            const enNodeDoc = node[nodeName];
-            const enNodeDocPath = path.join(originDocsDir, repairePath(enNodeDoc));
-            console.log(enNodeDocPath);
+    const enPluginDocNodesDir = path.join(enPluginDoc, 'docs');
+    const cnPluginDocNodesDir = path.join(cnPluginDoc, 'docs');
 
-            const _nodeDocPath = path.join(nodeDocsDir, `${repairePath(nodeName)}.md`);
-            const _enNodeDocPath = path.join(nodesDocsDir, 'en-US', pluginName, `${repairePath(nodeName)}.md`);
-            const _cnNodeDocPath = path.join(cnPluginDoc, `${repairePath(nodeName)}.md`);
-            console.log(_nodeDocPath);
+    const pluginNodesHome = plugin[pluginName][0]['Home']
+    const enPluginDocLicenses = plugin[pluginName][1]['Licenses']
 
-            if (!fs.existsSync(_nodeDocPath)) {
-                console.log('need add file:', nodeName);
-                if (!needAddFileContent[pluginName]) {
-                    needAddFileContent[pluginName] = [];
-                }
-                needAddFileContent[pluginName].push(nodeName);
-            } else {
-                if (!fs.existsSync(cnPluginDoc)) {
-                    fs.mkdirSync(cnPluginDoc, { recursive: true });
-                }
-                fs.copyFileSync(_nodeDocPath, _cnNodeDocPath)
-            }
+    if (!pluginNodesHome) continue
 
-            if (!fs.existsSync(enPluginDoc)) {
-                fs.mkdirSync(enPluginDoc, { recursive: true });
-            }
-            if (!fs.existsSync(_enNodeDocPath)) {
-              // zh-CN文件不存在
-              if (!fs.existsSync(_cnNodeDocPath)) {
-                needAddFileContent[pluginName].push(nodeName);
-              }
-            } else {
-              fs.copyFileSync(enNodeDocPath, _enNodeDocPath);
-            }
-        }
+    console.log( plugin)
+    console.log(pluginNodesHome)
+    console.log(enPluginDocLicenses)
+
+    // 创建文件夹、home、license
+    if (!fs.existsSync(enPluginDoc)) {
+      fs.mkdirSync(enPluginDoc, { recursive: true });
+    }
+    if (!fs.existsSync(cnPluginDoc)) {
+      fs.mkdirSync(cnPluginDoc, { recursive: true });
     }
 
-    // console.log(needAddFileContent);
-    fs.writeFileSync(needAddFile, JSON.stringify(needAddFileContent, null, 2));
+    if (!fs.existsSync(enPluginDocNodesDir)) {
+      fs.mkdirSync(enPluginDocNodesDir, { recursive: true });
+    }
+    if (!fs.existsSync(cnPluginDocNodesDir)) {
+      fs.mkdirSync(cnPluginDocNodesDir, { recursive: true });
+    }
+
+    fs.copyFileSync(path.join(originDocsDir, pluginNodesHome), path.join(enPluginDoc, 'index.md'));
+    fs.copyFileSync(path.join(originDocsDir, enPluginDocLicenses), path.join(enPluginDoc, 'licenses.md'));
+
+    fs.writeFileSync(path.join(cnPluginDoc, 'licenses.md'), enPluginDocLicenses);
+    fs.writeFileSync(path.join(cnPluginDoc, 'index.md'), pluginNodesHome);
+
+    // 获取node的名称
+    for (let j = 0; j < pluginNodes.length; j++) {
+      const node = pluginNodes[j];
+
+      const nodeName = Object.keys(node)[0];
+      const enNodeDoc = node[nodeName];
+      const enNodeDocPath = path.join(originDocsDir, repairePath(enNodeDoc));
+      // console.log(enNodeDocPath);
+
+      const _nodeDocPath = path.join(nodeDocsDir, `${repairePath(nodeName)}.md`);
+
+      const _enNodeDocPath = path.join(enPluginDocNodesDir, `${repairePath(nodeName)}.md`);
+      const _cnNodeDocPath = path.join(cnPluginDocNodesDir, `${repairePath(nodeName)}.md`);
+
+      if (!fs.existsSync(_nodeDocPath)) {
+        // console.log('need add file:', nodeName);
+        if (!needAddFileContent[pluginName]) {
+          needAddFileContent[pluginName] = [];
+        }
+        needAddFileContent[pluginName].push(nodeName);
+      } else {
+        if (!fs.existsSync(cnPluginDoc)) {
+          fs.mkdirSync(cnPluginDoc, { recursive: true });
+        }
+        fs.copyFileSync(_nodeDocPath, _cnNodeDocPath)
+      }
+
+      if (!fs.existsSync(enPluginDoc)) {
+        fs.mkdirSync(enPluginDoc, { recursive: true });
+      }
+      if (!fs.existsSync(_enNodeDocPath)) {
+        // zh-CN文件不存在
+        if (!fs.existsSync(_cnNodeDocPath)) {
+          needAddFileContent[pluginName].push(nodeName);
+        }
+      } else {
+        fs.copyFileSync(enNodeDocPath, _enNodeDocPath);
+      }
+    }
+  }
+
+  // console.log(needAddFileContent);
+  fs.writeFileSync(needAddFile, JSON.stringify(needAddFileContent, null, 2));
 }
 
 nodeGen(nav);
